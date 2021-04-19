a8dae7bd7657ecf1bee13d4a7c1b778faec42550
Alina <tyalinav@yandex.ru>
2021-04-19 23:57:09 +0300

    Добавлена поверхность для виджета

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    height: '100%'
+  },
+  paper: {
+    width: 'max-content',
+    height: 'max-content',
+    margin: 'auto',
+    padding: theme.spacing(4)
+  },
+  text: {
+    margin: 'auto'
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div></div>
+    <div className={classes.root}>
+      <Paper elevation={5} className={classes.paper}>
+      </Paper>
+    </div>
   );
 }
 
